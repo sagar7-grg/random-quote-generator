f7e5e57bea3b33e45f20218cca47f8703fe743e9
sagar7-grg <52279660+sagar7-grg@users.noreply.github.com>
2020-06-07 13:06:53 +0545

    indexfile
    certain mistakes has been improved.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 const API =
   "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
@@ -28,17 +29,42 @@ class App extends React.Component {
   }
 
   render() {
+    const tweet = `https://twitter.com/intent/tweet?text=${
+      this.state.random.quote
+    }-${this.state.random.author}`;
     return (
-      <div>
-        <button
-          type="button"
-          className="btn btn-success"
-          onClick={this.handleClick}
-        >
-          Next Quote
-        </button>
-        <h1>{this.state.random.quote}</h1>
-        <h1>{this.state.random.author}</h1>
+      <div className="container">
+        <div className="row">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{this.state.random.quote}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                -{this.state.random.author}
+              </h6>
+              <div className="d-flex flex-row bd-highlight mb-3 justify-content-between">
+                <div className="p-2 bd-highlight ">
+                  <a
+                    className="btn btn-primary "
+                    href={tweet}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Twitter
+                  </a>
+                </div>
+
+                <div className="p-2 bd-highlight">
+                  <button
+                    onClick={this.handleClick}
+                    className="btn btn-primary "
+                  >
+                    Next quote
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
